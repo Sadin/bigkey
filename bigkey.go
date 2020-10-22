6aee46f9cec25d2aa61b43d4f0f926211fbbf1d4
Zachary Snyder <contact@zsny.dev>
2020-10-21 23:30:48 -0400

    switched to zap logger

--- bigkey.go
+++ bigkey.go
@@ -2,59 +2,29 @@ package main
 
 import (
 	"flag"
-	"os"
 
 	"github.com/bwmarrin/discordgo"
-	log "github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 )
 
 func init() {
 	flag.StringVar(&token, "t", "", "Bot Token")
 	flag.Parse()
-	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.WarnLevel)
 }
 
 var token string
 
 func main() {
 
+	logger := zap.NewExample()
+	defer logger.Sync()
+
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
-		log.Fatalln(err)
+		logger.Info("failed auth to discord",
+			zap.String("err", err.Error()),
+		)
 	}
-
-	log.WithFields(log.Fields{
-		"animal": "walrus",
-		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
-
-	log.WithFields(log.Fields{
-		"omg":    true,
-		"number": 122,
-	}).Warn("The group's number increased tremendously!")
-
-	log.WithFields(log.Fields{
-		"omg":    true,
-		"number": 100,
-	}).Fatal("The ice breaks!")
-
-	// A common pattern is to re-use fields between logging statements by re-using
-	// the logrus.Entry returned from WithFields()
-	contextLogger := log.WithFields(log.Fields{
-		"common": "this is a common field",
-		"other":  "I also should be logged always",
-	})
-
-	contextLogger.Info("I'll be logged with common and other field")
-	contextLogger.Info("Me too")
 
 	dg.Close()
 }
