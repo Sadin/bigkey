3de030d09824152e7447e099af4f924e6d02a402
Zachary Snyder <contact@zsny.dev>
2020-10-21 23:51:26 -0400

    bot connects, info logs, init event handlers

--- bigkey.go
+++ bigkey.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"go.uber.org/zap"
@@ -13,18 +16,73 @@ func init() {
 }
 
 var token string
+var logger = zap.NewExample()
 
 func main() {
 
-	logger := zap.NewExample()
 	defer logger.Sync()
+
+	logger.Info(token)
 
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
-		logger.Info("failed auth to discord",
+		logger.Fatal("failed auth to discord",
 			zap.String("err", err.Error()),
 		)
 	}
 
+	dg.AddHandler(ready)
+
+	dg.AddHandler(guildCreate)
+
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates)
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		logger.Fatal("failed to open discord session",
+			zap.String("err", err.Error()),
+		)
+	}
+
+	logger.Info("bigkey is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
 	dg.Close()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+
+	s.UpdateStatus(0, "beeg keys")
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	logger.Info(m.Content,
+		zap.String("channelid", m.ChannelID),
+		zap.String("userid", m.Message.Author.ID),
+	)
+
+}
+
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+
+	if event.Guild.Unavailable {
+		return
+	}
+
+	for _, channel := range event.Guild.Channels {
+		if channel.ID == event.Guild.ID {
+			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			return
+		}
+	}
 }
